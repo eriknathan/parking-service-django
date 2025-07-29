2f9c9fcba3e8816e9bbe3d3f75d3282907d52dc7
eriknathan <eriknathan.contato@gmail.com>
2025-07-29 09:38:55 -0300

    Dockerizando e Subindo o Postgres na aplicação

--- core/settings.py
+++ core/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'parking_service',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'parking-service-db',
+        'PORT': '5432',
     }
 }
 
